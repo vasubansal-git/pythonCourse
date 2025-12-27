f93b8893e79203556c6064e61fe21f978eae51c6
vasubansal-git <vasubansal8907@gmail.com>
2025-12-27 22:56:28 +0530

    Data types

---
+++ dataTypes.py
@@ -0,0 +1,41 @@
+#  1-> what is variable?
+#  variable is like a container which stores the data.
+
+a = 123456789
+print(a)
+b = "vasu"
+print(b)
+
+a = 1
+b = True
+c = False
+d = "Vasu"
+e = None
+f = 1.1
+g = complex(8,2)
+print(g)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+#  (type) is used to print the type of the value of variable.
+
+# list (list is muttable means changable)
+list1 = [8, 2, 3, ["apple", "banana"]]
+print(list1)
+
+# tupple (tupple is immutable means unchangeable)
+tuple1= (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+# Mapped data: Dict
+# Dict: It is an unordered collection of data
+dict1 = {"name" : "vasu", "age" : 18, "Canvote" : True}
+print(dict1)
+
+
+#python m sab ik object hai!
