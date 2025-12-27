308e19fb2a70feaf5b55631187fe9e9b2cae7257
vasubansal-git <vasubansal8907@gmail.com>
2025-12-27 22:46:42 +0530

    Add comments, escape sequence and print statements

---
+++ second.py
@@ -0,0 +1,49 @@
+#Comments, Escape sequence, print Statement:
+
+#This is simgle-line comment
+# dont remove this! :comments (#) represents comments symbol
+ 
+#author: Vasu
+#course: 100 days of code!
+
+# print("Hey! I am a devil of my world, \nand this code viewer is also a devil.") (this is escape sequence character: \n)
+
+
+#this is a print statement
+print("This is a print statement!")
+
+#comment after print statement
+print("Hello world") #printing hello world
+
+#this is multi-line comment
+
+'''
+my name is vasu bansal
+my full name is vasu bansal.
+'''
+#we use tripple quotes(''' , """) for multi-line comment in python
+
+# print("hey!\n My name is vasu bansal.") \n is a escape sequence
+
+# print("Hey! I am a \"good boy\".\n My full name is vasu bansal") (\")is used to write two times double quode in a line.
+
+# print("hey", 6, 7, sep="-") (sep=)is used to seperate multiple things in a sentence by any character.
+
+print("Hey", 6, 7, sep="~", end="007")
+print("vasu")
+
+print("hey!", 6, 7, sep="-", end="\n")
+print("Vasu")
+
+'''
+
+p = int(input("Enter your number: "))
+if p > 0:
+    print("It is a positive number")
+elif p == 0:
+    print("It is 0")
+else:
+    print("It is a negative number")
+
+'''
+
