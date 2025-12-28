253445982eb7bd74644a82f649fc834197e0cde5
vasubansal-git <vasubansal8907@gmail.com>
2025-12-28 21:21:33 +0530

    First calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+print(5 + 7)
+print(14 - 7)
+print(15 / 7)
+print(15 // 7)
+print(2 * 7)
+print(6 % 4)
+print(2 ** 3)  #(**)to the power 2 * 2 * 2
+
+n = 2
+m = 4
+# ans1 = n + m
+print("addition of",n,"and",m,"is", n + m)
+print("subtraction of",n,"and",m,"is", n - m)
+print("Multiplication of",n,"and",m,"is", n * m)
+print("division of",n,"and",m,"is", n / m)
