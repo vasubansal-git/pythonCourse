baf691d6268aad8cd2791b6e2aec3779d72976ac
vasubansal-git <vasubansal8907@gmail.com>
2025-12-28 22:08:31 +0530

    Userinput with typecasting

---
+++ userInput.py
@@ -0,0 +1,19 @@
+# a = input("Enter your name: ")
+# print("My name is:",a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+print(x + y) #12100 bcoz input function takes input as string by default
+
+print("Sum is: ", int(x) + int(y)) #Explicit typecasting 112
+
+# a = input("Enter something: ")
+# b = input("Enter something else: ")
+
+# print("You enteres: ", a, b)
+# print(a + b)
+# print(a * b)
+# print(a - b)
+# print(a / b)
+
