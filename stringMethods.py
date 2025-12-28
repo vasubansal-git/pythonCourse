8fa3f08a8ccc0870f3e5846124261049366b27b0
vasubansal-git <vasubansal8907@gmail.com>
2025-12-28 23:49:28 +0530

    Add string methods

--- stringMethods.py
+++ stringMethods.py
@@ -1,5 +1,6 @@
 # String slicing 
 
+"""
 name = "Vasu bansal"
 print(len(name))  # length of string
 print(name[0:]) #all printed
@@ -17,3 +18,38 @@ print(name[-3:-1]) #sa ( -3 means 3rd last char and -1 means last char are exclu
 #quiz
 nm = "Harry"
 print(nm[-4:-2]) #ar
+
+"""
+
+#string 
+#Strings are immutable (unchangeable) in nature.
+
+a = "Vasu"
+b = "Vasu!!!!!!!!!!!!!!"
+c = "!!!!!Vasu!!!!! !!!!!!!!!! Sarthak"
+print(len(a))
+print(a.upper())
+print(a.lower())
+
+print(b.rstrip("!")) #rstrip() removes the trailing characters (characters at the end of a string)
+print(a.replace("Vasu", "Sarthak"))
+
+print(c.split(" ")) #split() method splits a string into a list.
+
+blogHeading = "Introduction tO Python"
+print(blogHeading.capitalize()) #capitalize() method converts the first character of a string to uppercase (if it is a letter).
+
+str1 = "Welcome to the console!!!"
+print(len(str1))
+str2 = str1.center(50) #center() method returns a centered string.
+print(len(str2))
+print(str2)
+
+print(a.count("a")) #count() method returns the number of occurrences of a substring in the given string.
+
+print(blogHeading.endswith("Python")) #True
+
+content = "This is a sample content with several words. This content is for testing."
+print(content.endswith("is", 0, 7)) #True
+
+print(content.find("is")) #2  (find() method returns the lowest index of the substring if it is found in given string. If it's not found then it returns -1)
