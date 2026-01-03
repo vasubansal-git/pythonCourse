9616346af11672dd1b7e3cba2c1b2b91ca18ed06
vasubansal-git <vasubansal8907@gmail.com>
2026-01-03 12:27:51 +0530

    Add if-else statements

---
+++ if_else_conditionalStatements.py
@@ -0,0 +1,25 @@
+"""
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+
+#Conditional operators: >, <, >=, <=, ==, !=
+print(a>18)
+print(a<18)
+print(a>=18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+
+if(a>18):
+    print("You can drive")
+else:
+    print("You cannot drive")
+
+"""
+
+applePrice = 210
+budget = 200
+if(applePrice <= budget):
+    print("Alexa, add 1kg apple to the cart")
+else:
+    print("Alexa, donot add apples to the cart")
