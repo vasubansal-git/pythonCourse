e3fe4b84c8235d210b698bf8ba0dffc522cac34b
vasubansal-git <vasubansal8907@gmail.com>
2026-01-07 12:22:21 +0530

    Add for loops

---
+++ loops.py
@@ -0,0 +1,23 @@
+#for loop:
+
+names = ["Vasu", "Sarthak", "Rekha", "Neeraj", "Vani"]
+for name in names:
+    print(name)
+
+for name in names:
+    print(name)
+    for i in name:
+        print(i)
+
+for i in range(5):
+    print(i)
+
+for i in range(1, 10):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
+
+#reverse loop
+for i in range(10, -1, -1):
+    print(i)
