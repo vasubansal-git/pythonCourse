34615edba524f6b38c1392b6b31888777a86b4ea
vasubansal-git <vasubansal8907@gmail.com>
2026-01-07 12:01:41 +0530

    Full file of match cases

--- matchCase.py
+++ matchCase.py
@@ -1,45 +1,45 @@
 # #Match case
 
-# x = int(input("Enter the value of x: "))
-# match x:
-#     case 0:
-#         print("Zero")
-#     case 1:
-#         print("One")
-#     case 2:
-#         print("Two")
-#     case 3:
-#         print("Three")
-#     case 4:
-#         print("Four")
-#     case _:
-#         print("Something else")
+x = int(input("Enter the value of x: "))
+match x:
+    case 0:
+        print("Zero")
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case 3:
+        print("Three")
+    case 4:
+        print("Four")
+    case _:
+        print("Something else")
 
 
 
-# username = input("Enter your username: ")
-# password = input("Enter your password: ")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
 
-# match (username, password):
-#     case ("admin", "1234"):
-#         print("Login successful")
-#     case ("admin", _):
-#         print("Wrong password")
-#     case (_, "1234"):
-#         print("Wrong username")
-#     case _:
-#         print("Wrong username and wrong password")
+match (username, password):
+    case ("admin", "1234"):
+        print("Login successful")
+    case ("admin", _):
+        print("Wrong password")
+    case (_, "1234"):
+        print("Wrong username")
+    case _:
+        print("Wrong username and wrong password")
 
 #combine match statements
 
-# day = int(input("Enter the day: "))
-# match day:
-#     case 1 | 2 | 3 | 4 | 5:
-#         print("Today is a weekday")
-#     case 6 | 7:
-#         print("Today is a weekend")
-#     case _:
-#         print("Invalid day")
+day = int(input("Enter the day: "))
+match day:
+    case 1 | 2 | 3 | 4 | 5:
+        print("Today is a weekday")
+    case 6 | 7:
+        print("Today is a weekend")
+    case _:
+        print("Invalid day")
 
 # combine match with if statements as guards
 
